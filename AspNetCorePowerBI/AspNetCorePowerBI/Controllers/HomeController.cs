6b00d418233c6157ed111d8ca89ccfe3ae39b307
Igor Nowicki <thesmilingcatofcheshire@gmail.com>
2023-04-25 01:36:55 +0200

    Move code from HomeController to PowerBiService

--- AspNetCorePowerBI/AspNetCorePowerBI/Controllers/HomeController.cs
+++ AspNetCorePowerBI/AspNetCorePowerBI/Controllers/HomeController.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Diagnostics;
+﻿using System.Diagnostics;
 using System.Threading.Tasks;
 using AspNetCorePowerBI.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using Microsoft.PowerBI.Api;
-using Microsoft.PowerBI.Api.Models;
-using Microsoft.Rest;
 
 namespace AspNetCorePowerBI.Controllers;
 
@@ -18,29 +14,13 @@ public class HomeController : Controller
     {
         _logger = logger;
     }
-    
+
     public async Task<IActionResult> Index([FromServices] PowerBISettings powerBISettings)
     {
-        var result = new PowerBIEmbedConfig { Username = powerBISettings.UserName };
-        var accessToken = await new PowerBiService().GetPowerBIAccessToken(powerBISettings);
-        var tokenCredentials = new TokenCredentials(accessToken, "Bearer");
-
-        using (var client = new PowerBIClient(new Uri(powerBISettings.ApiUrl), tokenCredentials))
-        {
-            var workspaceId = powerBISettings.WorkspaceId;
-            var reportId = powerBISettings.ReportId;
-            var report = await client.Reports.GetReportInGroupAsync(workspaceId, reportId);
-            var generateTokenRequestParameters = new GenerateTokenRequest("view");
-            var tokenResponse = await client.Reports.GenerateTokenAsync(workspaceId, reportId, generateTokenRequestParameters);
-
-            result.EmbedToken = tokenResponse;
-            result.EmbedUrl = report.EmbedUrl;
-            result.Id = report.Id;
-        }
+        var result = await new PowerBiService(powerBISettings).GetPowerBiEmbedConfig();
 
         return View(result);
     }
-
 
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
--- AspNetCorePowerBI/AspNetCorePowerBI/PowerBiService.cs
+++ AspNetCorePowerBI/AspNetCorePowerBI/PowerBiService.cs
@@ -3,12 +3,46 @@ using System.Collections.Generic;
 using System.Net.Http;
 using System.Threading.Tasks;
 using AspNetCorePowerBI.Models;
+using Microsoft.PowerBI.Api.Models;
+using Microsoft.PowerBI.Api;
+using Microsoft.Rest;
 using Newtonsoft.Json.Linq;
 
 namespace AspNetCorePowerBI;
 
 public class PowerBiService
 {
+    public PowerBiService(PowerBISettings settings)
+    {
+        _settings = settings;
+    }
+
+    private readonly PowerBISettings _settings;
+
+    public async Task<PowerBIEmbedConfig> GetPowerBiEmbedConfig()
+    {
+        var accessToken = await GetPowerBIAccessToken(_settings);
+        var tokenCredentials = new TokenCredentials(accessToken, "Bearer");
+
+        using var client = new PowerBIClient(new Uri(_settings.ApiUrl), tokenCredentials);
+
+        var workspaceId = _settings.WorkspaceId;
+        var reportId = _settings.ReportId;
+        var report = await client.Reports.GetReportInGroupAsync(workspaceId, reportId);
+        var generateTokenRequestParameters = new GenerateTokenRequest("view");
+        var tokenResponse = await client.Reports.GenerateTokenAsync(workspaceId, reportId, generateTokenRequestParameters);
+        
+        var result = new PowerBIEmbedConfig
+        {
+            Username = _settings.UserName,
+            EmbedToken = tokenResponse,
+            EmbedUrl = report.EmbedUrl,
+            Id = report.Id
+        };
+
+        return result;
+    }
+
     public async Task<string> GetPowerBIAccessToken(PowerBISettings powerBISettings)
     {
         using var client = new HttpClient();
